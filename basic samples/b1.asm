93ae047bfba1f8a40f80448a60d11d8fb06d57e7
theuniquefersa <holafersa@gmail.com>
2025-11-20 23:41:42 -0600

    feat: Initial commit

---
+++ basic samples/b1.asm
@@ -0,0 +1,12 @@
+  ORG     $8000
+
+  LDAA    $45        ; carga 8 bits (alto de 457C si era necesario)
+  LDAB    $7C        ; carga 8 bits (bajo de 457C)
+  LDAB    $31        ; valor válido para 8 bits (corrección)
+  LDD     $1789      ; carga 16 bits al registro D
+  LDX     $FDE8      ; 65000 decimal → FDE8h
+  ADDA    $7C        ; A = A + 7Ch
+  ANDA    $F0        ; solo 8 bits válidos para ANDA
+  LDY     $ABCD      ; carga 16 bits al registro Y
+
+  END
